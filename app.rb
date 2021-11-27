20c590ab00a41e888a86558031ccdefd35f8eeff
pofkuma <pof.kuma@gmail.com>
2021-11-27 09:56:22 +0900

    Modify file path for log anf db
    ログとメモデータを親ディレクトリ内に出力するように変更

--- app.rb
+++ app.rb
@@ -9,8 +9,8 @@ require 'erubi'
 require_relative 'memo_access_helper'
 require_relative 'route'
 
-LOG_FILE = '/tmp/sample-memo-app.log'
-PSTORE_FILE = '/tmp/sample-memo-app.pstore'
+LOG_FILE = "#{__dir__}/log/sample-memo-app.log"
+PSTORE_FILE = "#{__dir__}/db/sample-memo-app.pstore"
 PSTORE_NAME = 'memos'
 
 configure do
